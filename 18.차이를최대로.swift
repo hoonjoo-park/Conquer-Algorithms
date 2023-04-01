76773cf4c6dd9cbe4ff5fdaf3aea1986c713ab20
hoonjoo94 <hoonjoo94@naver.com>
2023-04-01 20:50:33 +0900

    백준 10819 차이를 최대로 풀이 완료

---
+++ 18.차이를최대로.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+func 차이를최대로() {
+    let n = Int(readLine()!)!
+    var nums = readLine()!.split(separator: " ").map { Int(String($0))! }
+    var maxSum = 0
+
+    func calcNums(_ arr: [Int]) -> Int {
+        var sum = 0
+        
+        for i in 0..<n-1 {
+            sum += abs(arr[i] - arr[i+1])
+        }
+        
+        return sum
+    }
+
+    func dfs(_ permutation: [Int], _ numsToTraverse: [Int]) -> Void {
+        for (i, num) in numsToTraverse.enumerated() {
+            var permutation = permutation
+            var numsToTraverse = numsToTraverse
+            
+            permutation.append(num)
+            numsToTraverse.remove(at: i)
+            
+            if permutation.count == nums.count {
+                maxSum = max(maxSum, calcNums(permutation))
+            }
+            
+            dfs(permutation, numsToTraverse)
+        }
+    }
+
+    dfs([], nums)
+
+    print(maxSum)    
+}
