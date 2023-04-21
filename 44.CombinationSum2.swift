5e5adbc5eb604e55f734798b3de0af301eb181de
hoonjoo94 <hoonjoo94@naver.com>
2023-04-21 21:25:51 +0900

    CombinationSumII 주석 추가 및 코드 리팩토링

--- 44.CombinationSum2.swift
+++ 44.CombinationSum2.swift
@@ -17,11 +17,10 @@ func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
             let current = sorted[i]
             var permutation = permutation
             
-            if i > index && sorted[i-1] == current { continue }
+            if (i > index && sorted[i-1] == current) || current > target { continue }
             
             permutation.append(sorted[i])
             backtracking(i+1, sum + sorted[i], permutation)
-            permutation.removeLast()
         }
     }
     
@@ -29,3 +28,14 @@ func combinationSum2(_ candidates: [Int], _ target: Int) -> [[Int]] {
     
     return answer
 }
+
+
+// candidates: [1, 1, 2, 5, 6, 7, 10], target: 8
+
+// backtracking(0, 0, []) 실행
+// permutation=[1] -> rest=[1, 2, 5, 6, 7, 10], index=0
+
+// backtracking(1, 1, [1]) 실행
+// premutation=[1, 1] -> [2, 5, 6, 7, 10], index=1
+// ❓여기서 1이 들어갈 수 있는 이유: i > index && sorted[i-1] == current) 조건 때문!
+// index는 1이고, 그에 따라 i도 1부터 시작하는데, 첫 시작때는 i와 index가 같기 때문에 1이 또 다시 들어갈 수 있는 것이다.
